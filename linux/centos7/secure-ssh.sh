edac2acd4f432360e70dc29b0b5368194a2d81e1
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-02-15 18:08:31 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -18,12 +18,12 @@ else
     mkdir -p /home/$1/.ssh
     cd /home/$1
     git clone https://github.com/ChampPG/test-repo/tree/main/linux/public-keys
-    cp ./home/$1/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
-    chmod 700 /home/$1/.ssh
-    chmod 600 /home/$1/.ssh/authorized_keys
-    chown $1:$1 /home/$1/.ssh
-    sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-    /etc/init.d/ssh restart
+    sudo cp /home/$1/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+    sudo chmod 700 /home/$1/.ssh
+    sudo chmod 600 /home/$1/.ssh/authorized_keys
+    sudo chown $1:$1 /home/$1/.ssh
+    sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+    sudo /etc/init.d/ssh restart
     echo "you're all set"
 fi
 
