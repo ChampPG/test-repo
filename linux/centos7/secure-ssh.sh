6621925c8e34c34ee5c384f9fca86f4090fbd5ec
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-02-15 18:20:28 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -5,9 +5,8 @@ echo "user $1"
 if [ -d "/home/$1/.ssh" ]
 then
     echo "Dir is there"
-    cd /home/$1
-    git clone https://github.com/ChampPG/test-repo/linux/public-keys
-    cp ./home/$1/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+    git pull
+    sudo cp /home/$uname/test-repo/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
     chmod 700 /home/$1/.ssh
     chmod 600 /home/$1/.ssh/authorized_keys
     chown -R $1:$1 /home/$1/.ssh
@@ -16,9 +15,8 @@ else
     echo "Dir isn't there"
     sudo useradd -m -d /home/$1 -s /bin/bash $1
     sudo mkdir -p /home/$1/.ssh
-    cd /home/$1/.ssh
-    git clone https://github.com/ChampPG/test-repo
-    sudo cp /linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+    git pull
+    sudo cp /home/$uname/test-repo/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
     sudo chmod 700 /home/$1/.ssh
     sudo chmod 600 /home/$1/.ssh/authorized_keys
     sudo chown -R $1:$1 /home/$1/.ssh
