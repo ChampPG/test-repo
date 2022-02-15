6d6eed40dd11a3cc832e48536c883ddb12b9c479
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-02-15 18:11:56 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -10,19 +10,19 @@ then
     cp ./home/$1/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
     chmod 700 /home/$1/.ssh
     chmod 600 /home/$1/.ssh/authorized_keys
-    chown $1:$1 /home/$1/.ssh
+    chown -R $1:$1 /home/$1/.ssh
     echo "You're all set!"
 else
     echo "Dir isn't there"
-    useradd -m -d /home/$1 -s /bin/bash $1
-    mkdir -p /home/$1/.ssh
+    sudo useradd -m -d /home/$1 -s /bin/bash $1
+    sudo mkdir -p /home/$1/.ssh
     cd /home/$1
     git clone https://github.com/ChampPG/test-repo/linux/public-keys
-    cp /home/$1/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
-    chmod 700 /home/$1/.ssh
-    chmod 600 /home/$1/.ssh/authorized_keys
-    chown $1:$1 /home/$1/.ssh
-    sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+    sudo cp /home/$1/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+    sudo chmod 700 /home/$1/.ssh
+    sudo chmod 600 /home/$1/.ssh/authorized_keys
+    sudo chown -R $1:$1 /home/$1/.ssh
+    sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
     /etc/init.d/ssh restart
     echo "you're all set"
 fi
