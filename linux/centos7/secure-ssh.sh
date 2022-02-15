cc596b5d241add9e2dbe4885123a72bf8b465039
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-02-14 20:33:00 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -6,25 +6,25 @@
 #adds a public key from the local repo or culed from the remote repo
 #removes roots ability to ssh in
 
-$uname = $1
-ssh keygen -t rsa -c "$uname"
+$name = $1
+ssh keygen -t rsa -c "$name"
 scp /home/$USER/.ssh/id_rsa.pub sys265@docker01-paul:
 
 ssh sys265@docker01-paul
 
-if [ ! -d "/home/$uname/.ssh"]
+if [ ! -d "/home/$name/.ssh"]
 then
-    useradd -m -d /home/$uname -s /bin/bash $uname
-    mkdir /home/$uname/.ssh
-    cd /home/$uname
-    cp ./home/sys265/id_rsa.pub /home/$uname/.ssh/authorized_keys
-    chmod 700 /home/$uname/.ssh
-    chmod 600 /home/$uname/.ssh/authorized_keys
-    chown $uname:$uname /home/$uname/.ssh
+    useradd -m -d /home/$name -s /bin/bash $name
+    mkdir /home/$name/.ssh
+    cd /home/$name
+    cp ./home/sys265/id_rsa.pub /home/$name/.ssh/authorized_keys
+    chmod 700 /home/$name/.ssh
+    chmod 600 /home/$name/.ssh/authorized_keys
+    chown $uname:$name /home/$name/.ssh
     sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
     /etc/init.d/ssh restart
     exit
-    ssh $uname@docker01-paul
+    ssh $name@docker01-paul
 fi
     exit
-    ssh $uname@docker01-paul
+    ssh $name@docker01-paul
