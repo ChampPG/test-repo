5bc8f23fecfbe99290997b62fed8d4b2f4a77a58
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-02-14 20:30:14 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -6,8 +6,12 @@
 #adds a public key from the local repo or culed from the remote repo
 #removes roots ability to ssh in
 
+$uname = $1
+ssh keygen -t rsa -c "$uname"
+scp /home/$USER/.ssh/id_rsa.pub sys265@docker01-paul:
+
 ssh sys265@docker01-paul
-$uname=$1
+
 $DIR="/home/$uname"
 if [-d "$DIR"]
 then
@@ -17,8 +21,7 @@ else
     useradd -m -d /home/$uname -s /bin/bash $uname
     mkdir /home/$uname/.ssh
     cd /home/$uname
-    git clone https://github.com/ChampPG/test-repo/linux/public-keys
-    cp ./test-repo/id_rsa.pub /home/$uname/.ssh/authorized_keys
+    cp ./home/sys265/id_rsa.pub /home/$uname/.ssh/authorized_keys
     chmod 700 /home/$uname/.ssh
     chmod 600 /home/$uname/.ssh/authorized_keys
     chown $uname:$uname /home/$uname/.ssh
