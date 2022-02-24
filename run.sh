a380fa8eff3204a20758e17bfd7048909b90acf6
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-02-24 00:16:33 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -37,36 +37,38 @@ scp websrv.csr root@$1:/etc/pki/CA
 #ssh CA
 ssh root@$1
 
-#prep Cert
-cd /etc/pki/CA
-touch index.txt
-echo 1000 > serial
+if [$3=1]
+then
+  #prep Cert
+  cd /etc/pki/CA
+  touch index.txt
+  echo 1000 > serial
 
-#gen CA key and pem
-openssl genrsa -des3 -out private/cakey.pem 2048
-openssl req -new -x509 -days 365 -key private/cakey.pem -out cacert.pem
+  #gen CA key and pem
+  openssl genrsa -des3 -out private/cakey.pem 2048
+  openssl req -new -x509 -days 365 -key private/cakey.pem -out cacert.pem
 
-#gen websrv.crt
-openssl ca -out websrv.crt -infiles websrv.csr
-scp websrv.crt paul@$2
+  #gen websrv.crt
+  openssl ca -out websrv.crt -infiles websrv.csr
+  scp websrv.crt paul@$2
+fi
 
-#return to websrv
-exit
+if[$3=2]
+then
+  #get key and cert copied
+  cd /home/paul
+  cp websrv.crt /etc/pki/tls/certs/websrv.crt
+  cp websrv.key /etc/pki/tls/private/websrv.key
 
-#get key and cert copied
-cd /home/paul
-cp websrv.crt /etc/pki/tls/certs/websrv.crt
-cp websrv.key /etc/pki/tls/private/websrv.key
+  #install mod_ssl for https
+  yum -y install mod_ssl
 
-#install mod_ssl for https
-yum -y install mod_ssl
+  echo 'Now update /etc/httpd/conf.d/ssl.conf'
+  echo 'find SSLCertificateFile'
+  echo 'find SSLCertificateKeyFile'
+  echo 'then restart httpd from root'
+  sleep 10
 
-echo 'Now update /etc/httpd/conf.d/ssl.conf'
-echo 'find SSLCertificateFile'
-echo 'find SSLCertificateKeyFile'
-echo 'then restart httpd from root'
-sleep 10
-
-#edit SSLCerticateFile and SSLCertificateKeyFile
-vi /etc/httpd/conf.d/ssl.conf
-
+  #edit SSLCerticateFile and SSLCertificateKeyFile
+  vi /etc/httpd/conf.d/ssl.conf
+fi
