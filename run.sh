bb9af752148af65ddaaa6cf13f7d7e85822a7847
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-02-24 23:03:03 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,7 +21,6 @@ then
 
   # httpd install and firewall
   systemctl enable httpd
-  sudo firewall-cmd --permanent --add-port=80/tcp
   sudo firewall-cmd --permanent --add-port=443/tcp
   sudo firewall-cmd --reload
   sudo systemctl start httpd
