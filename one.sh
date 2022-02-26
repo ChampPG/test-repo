6a634964b4aee9dde99e950ef03b6df6f04c0a72
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-02-26 16:55:14 -0500

    Update one.sh

--- one.sh
+++ one.sh
@@ -43,10 +43,9 @@ else
   
   ssh root@$caip "cd /etc/pki/CA ; touch /etc/pki/CA/index.txt ; echo $num > /etc/pki/CA/serial ; openssl genrsa -des3 -out /etc/pki/CA/private/cakey.pem 2048 ; openssl req -new -x509 -days $days -key /etc/pki/CA/private/cakey.pem -out /etc/pki/CA/cacert.pem ; openssl ca -out /etc/pki/CA/websrv.crt -infiles /etc/pki/CA/websrv.csr"
   
-  user = 'paul'
 
 ssh root@$caip << EOF 
- scp /etc/pki/CA/websrv.crt $user@$webip:/home/paul
+ scp /etc/pki/CA/websrv.crt paul@$webip:/home/paul
 EOF
   
   #get key and cert copied
